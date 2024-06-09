7d5e3d358e4ecdbc3eeb7e0491f600d002f02617
vincent verlaan <vincentverlaan33@outlook.fr>
2024-06-09 21:36:51 +0200

    Add body parser and working post with SQL

--- restaurants/restaurants.js
+++ restaurants/restaurants.js
@@ -1,5 +1,12 @@
 const express = require("express");
+const { Client } = require('pg');
 const app = express();
+const database_postgres = new Client({user: 'user',password: 'mysecretpassword',host: '0.0.0.0',port: '8001',database: 'CESIEats',});
+
+database_postgres.connect()
+.then(() => {
+    console.log('Connected to PostgreSQL database');
+});
 
 app.get("/", (req,res)=>{
     res.send("Restaurants default route")
@@ -7,4 +14,16 @@ app.get("/", (req,res)=>{
 
 app.listen(3001, ()=>{
     console.log("Up and running restaurants");
+})
+
+app.get("/restaurants", (req, res) => {
+    database_postgres.query('SELECT * FROM restaurants', (err, result)=>{
+        if (err) {
+            console.error('Error executing query', err);
+        }
+        else {
+            console.log('Query result :', result.rows);
+        }
+        res.send(result.rows);
+    });
 })
